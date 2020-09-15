501be3142691ec6b4eb3c5908d1d1e79e1468dab
Brown Nguyễn <brownnguyen@gmail.com>
2020-09-15 08:57:22 +0700

    add_top

--- js/index.js
+++ js/index.js
@@ -2,5 +2,17 @@ $('.slide-content').slick({
     arrows: false
 });
 $('.what-case').slick({
-    arrows: false
+    arrows: true,
+    dots: true
+});
+let arrowLeft = document.querySelector('.what-case .slick-prev');
+let arrowRight = document.querySelector('.what-case .slick-next');
+arrowLeft.innerHTML = '<span class="arrowLeft"></span>';
+arrowRight.innerHTML = '<span class="arrowRight"></span>';
+console.log(arrowRight);
+var Scroll = document.querySelector('.scroll');
+$('#scroll').click(function () {
+    $('html, body').animate({
+        scrollTop: $("#what").offset().top
+    }, 1000);
 });
